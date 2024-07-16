9f554635c8e4dad69ff29ee8acc9ff8e114d9911
Danny Murray <murraydp@mail.uc.edu>
2024-07-16 17:23:07 -0400

    Set lint to ignore webpack config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import pluginJs from "@eslint/js";
 
 
 export default [
+	{
+		ignores: [ "*.config.js" ],
+	},
 	{languageOptions: { globals: globals.browser }},
 	pluginJs.configs.recommended,
 	{
